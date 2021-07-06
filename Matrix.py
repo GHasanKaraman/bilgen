f27ec0a15b6264a9bdb3e61018430d97e756e22e
Gurkan <ghasankaraman@users.noreply.github.com>
2021-07-06 18:56:08 +0300

    matrix validation

--- Matrix.py
+++ Matrix.py
@@ -1,5 +1,19 @@
+from bilgen.Errors import MatrixError
+
 class Matrix:
     def __init__(self, array):
+        row = len(array)
+        col = 0
+        if type(array[0]) is list:
+            col = len(array[0])
+        else:
+            raise MatrixError("Creating a matrix from ragged nested sequences (which is a list matrices with different lengths or shapes) is deprecated.")
+        for item in array:
+            if type(item) is list:
+                if not len(item) == col:
+                    raise MatrixError("Creating a matrix from ragged nested sequences (which is a list matrices with different lengths or shapes) is deprecated.")
+            else:
+                raise MatrixError("Creating a matrix from ragged nested sequences (which is a list matrices with different lengths or shapes) is deprecated.")
         self.array = array
 
     def __repr__(self):
