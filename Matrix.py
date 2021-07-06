9cdef213860437efba708ff9dbc6a76686e5cae4
Gurkan <ghasankaraman@users.noreply.github.com>
2021-07-06 17:10:27 +0300

    basic operator overloading methods initialized

---
+++ Matrix.py
@@ -0,0 +1,15 @@
+class Matrix:
+    def __init__(self, array):
+        self.array = array
+
+    def __repr__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+    
+    def __setitem__(self, index, value):
+        pass
