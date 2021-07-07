c851447d763438967eee4901832e89e73902d70c
alicancv <alicansucu@outlook.com>
2021-07-07 15:42:03 +0300

    linalg sub function was added

--- Matrix/linalg.py
+++ Matrix/linalg.py
@@ -26,8 +26,19 @@ def add(matrix1, matrix2):
         raise LinAlgError("The dimensions of the two given matrices must be the same")
 
 def sub(matrix1, matrix2):
-    pass
-
+    if(matrix1.shape == matrix2.shape):
+        tmp_matrix = []
+        for i in range(matrix1.shape[0]):
+            tmp_array = []
+            for j in range(matrix1.shape[1]):
+                tmp_array.append(matrix1[i,j] - matrix2[i, j])
+            
+            tmp_matrix.append(tmp_array) 
+        
+        return Matrix(tmp_matrix)
+    else:
+        raise LinAlgError("The dimensions of the two given matrices must be the same")
+    
 def inv(matrix):
     pass
 
