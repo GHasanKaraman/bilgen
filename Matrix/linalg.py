cccda4bf2c23f8db5f2a7cb73250d17815da9a3d
Gurkan <ghasankaraman@users.noreply.github.com>
2021-07-07 14:40:26 +0300

    prototype functions

---
+++ Matrix/linalg.py
@@ -0,0 +1,22 @@
+from bilgen.Matrix.Array import Matrix
+
+def transpose(matrix):
+    pass
+
+def dot(matrix1, matrix2):
+    pass
+
+def add(matrix1, matrix2):
+    pass
+
+def sub(matrix1, matrix2):
+    pass
+
+def inv(matrix):
+    pass
+
+def det(matrix):
+    pass
+
+def norm(matrix):
+    pass
